216e683605d6dd642c262797964499ff4b78c67a
Nathan Danielson <nbd2111@mail.cumc.columbia.edu>
2014-05-22 22:46:07 -0400

    setup script for building ROI buddy Windows distribution

--- ROI_BuddyUI/setup.py
+++ ROI_BuddyUI/setup.py
@@ -1,18 +1,33 @@
-from setuptools import setup
-# import py2exe
+#NOTE: NEED TO COPY .DLLs FROM SHAPELY INTO THE DIST DIRECTORY AFTER BUILDING
+from guidata import disthelpers as dh
+from shutil import copy, copytree
+from os.path import join
 
-setup(
-    name="ROIBuddy",
-    version="0.1.0",
-    author_email="software@losonczylab.org",
-    url="http://www.losonczylab.org/roibuddy",
-    license="GNU General Public License (GPL)",
-    packages=['ROIBuddy'],
-    package_dir={'ROIBuddy': '.'},
-    # packages=['liftr'],
-    # package_data={"liftr": ["ui/*"]},
-    # scripts=["bin/liftr"],
-    # windows=[{"script": "bin/liftr"}],
-    # options={"py2exe": {"skip_archive": True, "includes": ["sip"]}},
-    scripts=['ROIBuddy']
-)
+TARGET_DIR = './dist'
+
+INCLUDES = ['numpy', 'cPickle', 'sima', 'sima.iterables', 'sima.ROI', 'sima.segment', 'sima.imaging',
+            'scipy', 'scipy.cluster.vq', 'scipy.misc', 'scipy.ndimage',
+            'scipy.ndimage.filters', 'skimage', 'skimage.color', 'skimage.draw',
+            'skimage.feature', 'skimage.filter', 'skimage.measure', 'skimage.morphology',
+            'skimage._shared.interpolation',
+            'skimage.filter.rank.core_cy', 'scipy.special._ufuncs_cxx', 'skimage._shared.geometry',
+            'scipy.sparse.csgraph._validation', 'skimage._shared.transform',
+            'skimage.transform', 'sip', 'PyQt4', 'PyQt4.QtSvg', 'guidata', 'guiqwt']
+
+dist = dh.Distribution()
+
+dist.setup(name='ROI_Buddy',
+           version='0.1',
+           description='ROI Buddy GUI',
+           script='roi_buddy.py',
+           target_name='ROI_Buddy.exe',
+           target_dir=TARGET_DIR,
+           icon='icon.ico',
+           includes=INCLUDES)
+
+dist.add_modules('guidata', 'guiqwt')
+dist.build_py2exe()
+
+copy('geos.dll', TARGET_DIR)
+copy('geos_c.dll', TARGET_DIR)
+copytree('icons', join(TARGET_DIR, 'icons'))
