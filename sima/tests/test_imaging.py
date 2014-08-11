07bbfccf707f0daf74821f4230228ca5885e9457
Jack Bowler <jcb2238@columbia.edu>
2014-08-11 14:45:27 -0400

    annotations added, unit tests for motion.py added

--- sima/tests/test_imaging.py
+++ sima/tests/test_imaging.py
@@ -1,3 +1,9 @@
+# Unit tests for sima/imaging.py
+# Tests follow conventions for NumPy/SciPy avialble at 
+# https://github.com/numpy/numpy/blob/master/doc/TESTS.rst.txt
+
+# use assert_() and related functions over the built in assert to ensure tests
+# run properly, regaurdless of how python is started.
 from numpy.testing import run_module_suite
 from numpy.testing import (assert_, assert_equal, assert_almost_equal,
         assert_array_almost_equal, assert_raises, assert_array_equal,
--- sima/tests/test_normcut.py
+++ sima/tests/test_normcut.py
@@ -1,3 +1,9 @@
+# Unit tests for sima/normcut.py
+# Tests follow conventions for NumPy/SciPy avialble at 
+# https://github.com/numpy/numpy/blob/master/doc/TESTS.rst.txt
+
+# use assert_() and related functions over the built in assert to ensure tests
+# run properly, regaurdless of how python is started.
 from numpy.testing import (assert_, assert_equal, assert_almost_equal,
         assert_array_almost_equal, assert_raises, assert_array_equal,
         dec, TestCase, run_module_suite, assert_allclose)
@@ -12,9 +18,6 @@ def setup():
     return
 
 def teardown():
-    return
-
-def test_extract_rois():
     return
 
 if __name__ == "__main__":
