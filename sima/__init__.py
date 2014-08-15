5dd8e7d869d619cccb060211fa24b6d8c1283f2f
Patrick Kaifosh <pwk2108@columbia.edu>
2014-08-15 12:22:44 -0400

    Removed whitesapce around sima docstring.

--- sima/__init__.py
+++ sima/__init__.py
@@ -1,11 +1,9 @@
-"""
-SIMA: Python package for sequential image analysis.
+"""SIMA: Python package for sequential image analysis.
 Developed by Patrick Kaifosh, Jeffrey Zaremba, Nathan Danielson.
 Copyright (C) 2014 The Trustees of Columbia University in the City of New York.
 Licensed under the GNU GPL version 2 or later.
 Documentation: http://www.losonczylab.org/sima
-Version 0.1.2-alpha
-"""
+Version 0.1.2-alpha"""
 print __doc__
 
 from sima.imaging import ImagingDataset
