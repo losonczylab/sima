14a1ff09b16bfbd09bb564875af79b77192ef3bf
Patrick Kaifosh <pwk2108@columbia.edu>
2014-08-20 11:07:43 -0400

    Fixed windows setup script for ROI Buddy.

--- ROI_BuddyUI/setup-windows-build.py
+++ ROI_BuddyUI/setup-windows-build.py
@@ -3,7 +3,7 @@
 Setup script for building the ROI Buddy GUI
 
 Things to note:
--- This should be build from a machine with a patched version of guiqwt.baseplot.py:
+-- This should be built from a machine with a patched version of guiqwt.baseplot.py:
         def add_item_with_z_offset(self, item, zoffset):
         ""
         Add a plot *item* instance within a specified z range, over *zmin*
@@ -17,10 +17,11 @@ Things to note:
             z = zlist[dzlist[0,0]]+1
         self.add_item(item, z=z)
 
--- Due to a shapely error, the geos.dll and geos_c.dll files need to be copied into dist manually
+-- Due to a shapely error, the geos.dll and geos_c.dll files need to be copied
+   into dist manually
 
-
---CTYPES PATCH HERE
+-- It seems it is not possible to package Libtiff, so this import must be
+   manually removed from sima.iterables
 """
 
 from guidata import disthelpers as dh
@@ -30,14 +31,18 @@ import zipfile as zf
 
 TARGET_DIR = './dist'
 
-INCLUDES = ['numpy', 'cPickle', 'sima', 'sima.iterables', 'sima.ROI', 'sima.segment', 'sima.imaging',
-            'scipy', 'scipy.cluster.vq', 'scipy.misc', 'scipy.ndimage',
-            'scipy.ndimage.filters', 'skimage', 'skimage.color', 'skimage.draw',
-            'skimage.feature', 'skimage.filter', 'skimage.measure', 'skimage.morphology',
-            'skimage._shared.interpolation',
-            'skimage.filter.rank.core_cy', 'scipy.special._ufuncs_cxx', 'skimage._shared.geometry',
+INCLUDES = ['numpy', 'cPickle', 'sima', 'sima.iterables', 'sima.ROI',
+            'sima.segment', 'sima.imaging', 'scipy', 'scipy.cluster.vq',
+            'scipy.misc', 'scipy.ndimage', 'scipy.ndimage.filters', 'skimage',
+            'skimage.color', 'skimage.draw', 'skimage.feature',
+            'skimage.filter', 'skimage.measure', 'skimage.morphology',
+            'skimage._shared.interpolation', 'skimage.filter.rank.core_cy',
+            'scipy.special._ufuncs_cxx', 'skimage._shared.geometry',
             'scipy.sparse.csgraph._validation', 'skimage._shared.transform',
-            'skimage.transform', 'sip', 'PyQt4', 'PyQt4.QtSvg', 'guidata', 'guiqwt']
+            'skimage.transform', 'sip', 'PyQt4', 'PyQt4.QtSvg', 'guidata',
+            'guiqwt', 'h5py', 'h5py.defs', 'h5py.utils', 'h5py.h5ac',
+            'h5py._proxy', 'h5py._conv', 'h5py._errors', 'h5py._objects',
+            'h5py.h5']
 
 dist = dh.Distribution()
 
