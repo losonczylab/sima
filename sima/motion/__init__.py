1dad0e40bc708e88d1346ca7ba8c2e327f4281d3
Patrick Kaifosh <pwk2108@columbia.edu>
2014-11-05 17:13:45 -0500

    Reducing redundancy between 2D & 3D motion correction

--- sima/motion/__init__.py
+++ sima/motion/__init__.py
@@ -2,7 +2,6 @@ from numpy.testing import Tester
 test = Tester().test
 
 from motion import MotionEstimationStrategy
-from align3d import VolumeTranslation
-from frame_align import PlaneTranslation2D
+from frame_align import PlaneTranslation2D, VolumeTranslation
 from _hmm import HiddenMarkov2D, MovementModel
 from hmm3d import HiddenMarkov3D
