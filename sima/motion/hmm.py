a1a0829a2337328a961f9ac4c35c6a5154cd6357
Patrick Kaifosh <pwk2108@columbia.edu>
2015-01-12 19:30:43 -0500

    Bug fix in motion correction.

--- sima/motion/hmm.py
+++ sima/motion/hmm.py
@@ -469,6 +469,7 @@ class MovementModel(object):
     def __init__(self, cov_matrix, U, s, mean_shift):
         if not np.all(np.isfinite(cov_matrix)):
             raise ValueError
+        assert np.linalg.det(cov_matrix) > 0
         self._cov_matrix = cov_matrix
         self._U = U
         self._s = s
@@ -528,9 +529,10 @@ class MovementModel(object):
                           [coefficients[3], coefficients[4], coefficients[5]]])
         cov_matrix = np.cov(future.T - np.dot(A, past.T))
         # make cov_matrix non-singular
-        Uc, sc, Vc = np.linalg.svd(A)
+        Uc, sc, _ = np.linalg.svd(cov_matrix)  # NOTE: U == V
         sc = np.maximum(sc, 1. / len(shifts))
-        cov_matrix = np.dot(Uc, np.dot(np.diag(sc), Vc))
+        cov_matrix = np.dot(Uc, np.dot(np.diag(sc), Uc))
+        assert np.linalg.det(cov_matrix) > 0
         U, s, _ = np.linalg.svd(A)  # NOTE: U == V for positive definite A
         assert np.max(s) < 1
         return cls(cov_matrix, U, s, mean_shift)
@@ -577,6 +579,7 @@ class MovementModel(object):
 
         """
         cov_matrix = self.cov_matrix(dt)
+        assert np.linalg.det(cov_matrix) > 0
         log_transition_probs = lambda x: -0.5 * (
             np.log(2 * np.pi * np.linalg.det(cov_matrix)) +
             np.dot(x, np.linalg.solve(cov_matrix, x)))
