Improvements and fixes to ROI extraction: -BUG FIX: results were improperly scaling with the inverse of the number of pixels in an ROI. This won't affect dff results, but did give incorrect raw values. -BUG FIX: ROIs were not properly setting the im_shape during extraction, could lead to dropped planes -BUG FIX: polygon and boolean mask ROIs were being normalized before removing overlapping pixels, so results were too low. -extraction run with one procesor no longer uses pools -added test cases for all basic extraction functionality -minor speedup to extraction, prevents unnecessary recalculation

sima/extract.py
@@ -106,10 +106,10 @@ def _roi_extract(inputs):
         return final_values
 
     n_rois = constants['A'].shape[1]
-    masked_pixels = constants['masked_pixels']
+    masked_frame = frame[constants['masked_pixels']]
 
     # Determine which pixels and ROIs were imaged this frame
-    imaged_pixels = np.isfinite(frame[masked_pixels])
+    imaged_pixels = np.isfinite(masked_frame)
 
     # If there is overlapping pixels between the ROIs calculate the full
     # pseudoinverse of A, if not use a shortcut
@@ -136,10 +136,11 @@ def _roi_extract(inputs):
         imaged_masks.data **= 2
         scale_factor = orig_masks.sum(axis=1) / imaged_masks.sum(axis=1)
         scale_factor = np.array(scale_factor).flatten()
-        weights = diags(scale_factor, 0) * imaged_masks
+        weights = diags(scale_factor, 0) \
+            * constants['mask_stack'][imaged_rois][:, imaged_pixels]
 
     # Extract signals
-    values = weights * frame[masked_pixels][imaged_pixels, np.newaxis]
+    values = weights * masked_frame[imaged_pixels, np.newaxis]
     weights_sums = weights.sum(axis=1)
     result = values + weights_sums
 
@@ -150,7 +151,7 @@ def _roi_extract(inputs):
         return (frame_idx, result, None)
 
     # Same as 'values' but with the demixed frame data
-    demixed_frame = frame[masked_pixels] + constants['demixer']
+    demixed_frame = masked_frame + constants['demixer']
     demixed_values = weights * demixed_frame[imaged_pixels, np.newaxis]
     demixed_result = demixed_values + weights_sums
 
@@ -312,15 +313,9 @@ def extract_rois(dataset, rois, signal_channel=0, remove_overlap=True,
     num_planes, num_rows, num_columns, num_channels = dataset.frame_shape
 
     for roi in rois:
-        roi.im_shape = (num_rows, num_columns)
+        roi.im_shape = (num_planes, num_rows, num_columns)
     masks = [hstack([mask.reshape((1, num_rows * num_columns))
              for mask in roi.mask]) for roi in rois]
-
-    # If mask is boolean convert to float and normalize values such that
-    # the sum of the weights in each ROI is 1
-    for mask_idx, mask in it.izip(it.count(), masks):
-        if mask.dtype == bool:
-            masks[mask_idx] = mask.astype('float') / mask.nnz
 
     # Find overlapping pixels
     overlap = _identify_overlapping_pixels(masks)
@@ -328,6 +323,12 @@ def extract_rois(dataset, rois, signal_channel=0, remove_overlap=True,
     # Remove pixels that overlap between ROIs
     if remove_overlap:
         masks = _remove_pixels(masks, overlap)
+
+    # If mask is boolean convert to float and normalize values such that
+    # the sum of the weights in each ROI is 1
+    for mask_idx, mask in it.izip(it.count(), masks):
+        if mask.dtype == bool:
+            masks[mask_idx] = mask.astype('float') / mask.nnz
 
     # Identify non-empty ROIs
     original_n_rois = len(masks)
@@ -391,12 +392,23 @@ def extract_rois(dataset, rois, signal_channel=0, remove_overlap=True,
         # This will farm out signal extraction across 'n_pools' CPUs
         # The actual extraction is in _roi_extract, it's a separate
         # top-level function due to Pool constraints.
+        if n_pools > 1:
+            map_generator = pool.imap_unordered(_roi_extract, it.izip(
+                _data_chunker(
+                    iter(sequence), dataset.time_averages, signal_channel),
+                it.count(), it.repeat(constants)), chunksize=chunksize)
+        else:
+            map_generator = it.imap(_roi_extract, it.izip(
+                _data_chunker(
+                    iter(sequence), dataset.time_averages, signal_channel),
+                it.count(), it.repeat(constants)))
 
-        for frame_idx, raw_result, demix_result in pool.imap_unordered(
-                _roi_extract, it.izip(
-                    _data_chunker(
-                        iter(sequence), dataset.time_averages, signal_channel),
-                    it.count(), it.repeat(constants)), chunksize=chunksize):
+        # Loop over generator and extract signals
+        while True:
+            try:
+                frame_idx, raw_result, demix_result = next(map_generator)
+            except StopIteration:
+                break
 
             signal[:, frame_idx] = np.array(raw_result).flatten()
             if demixer is not None:

sima/tests/test_extract.py
@@ -1,9 +1,9 @@
 # Unit tests for sima/extract.py
-# Tests follow conventions for NumPy/SciPy avialble at
+# Tests follow conventions for NumPy/SciPy available at
 # https://github.com/numpy/numpy/blob/master/doc/TESTS.rst.txt
 
 # use assert_() and related functions over the built in assert to ensure tests
-# run properly, regaurdless of how python is started.
+# run properly, beardless of how python is started.
 from numpy.testing import (
     assert_,
     assert_equal,
@@ -16,9 +16,8 @@ from numpy.testing import (
     run_module_suite,
     assert_allclose)
 
-from sima import extract
-import os
-import tempfile
+from scipy.weave import build_tools
+import sima
 import numpy as np
 
 
@@ -30,8 +29,192 @@ def teardown():
     return
 
 
-def test_extract_rois():
-    return
+class Test_ConstantData(object):
+    def setup(self):
+        data = np.ones((10, 2, 6, 8, 2))
+        data[:, 0, 1:4, 2:6, 1] = 1000
+
+        seq = sima.Sequence.create('ndarray', data)
+        self.dataset = sima.ImagingDataset([seq], savedir=None)
+
+    def test_polygon_roi(self):
+        roi = sima.ROI.ROI(
+            polygons=[[2, 1, 0], [6, 1, 0], [6, 4, 0], [2, 4, 0]],
+            im_shape=(2, 6, 8))
+        rois = sima.ROI.ROIList([roi])
+        signals = self.dataset.extract(
+            rois=rois, signal_channel=1, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+
+        assert_equal(signals['raw'][0], 1.)
+
+    def test_boolean_mask_roi(self):
+        mask = np.array([[[False, False, False, False, False, False],
+                          [False, False, True, True, True, True],
+                          [False, False, True, True, True, True],
+                          [False, False, True, True, True, True]]])
+        roi = sima.ROI.ROI(mask=mask)
+        rois = sima.ROI.ROIList([roi])
+        signals = self.dataset.extract(
+            rois=rois, signal_channel=1, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+
+        assert_array_equal(signals['raw'], 1.)
+
+    def test_non_boolean_mask(self):
+        mask = np.array([[[0., 0., 0., 0., 0., 0.],
+                          [0., 0., 0.4, 0.3, 0.2, 0.1],
+                          [0., 0., 0.4, 0.3, 0.2, 0.1],
+                          [0., 0., 0.4, 0.3, 0.2, 0.1]]])
+        roi = sima.ROI.ROI(mask=mask)
+        rois = sima.ROI.ROIList([roi])
+        signals = self.dataset.extract(
+            rois=rois, signal_channel=1, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+
+        assert_array_equal(signals['raw'][0], 3.)
+
+    def test_overlapping_rois(self):
+        polygon = [[2, 1, 0], [5, 1, 0], [5, 3, 0], [2, 3, 0]]
+        mask = np.array([[[False, False, False, False, False, False],
+                          [False, False, False, False, False, False],
+                          [False, False, False, True, True, True],
+                          [False, False, False, True, True, True]]])
+        rois = sima.ROI.ROIList(
+            [sima.ROI.ROI(polygons=polygon, im_shape=(2, 6, 8)),
+             sima.ROI.ROI(mask=mask)])
+        no_overlap_signals = self.dataset.extract(
+            rois=rois, signal_channel=1, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+        overlap_signals = self.dataset.extract(
+            rois=rois, signal_channel=1, remove_overlap=False, n_processes=2,
+            demix_channel=None)
+
+        assert_array_equal(no_overlap_signals['raw'], 1.)
+        assert_array_equal(overlap_signals['raw'], 1.)
+
+
+class Test_VaryingData(object):
+    def setup(self):
+        pattern = np.tile([0.4, 0.8, 1.2, 1.6], (3, 1))
+        data = np.ones((20, 2, 6, 8, 2))
+        for t in range(20):
+            data[t, 1, 1:4, 2:6, 0] = np.roll(pattern, t, 1) * 1000
+
+        seq = sima.Sequence.create('ndarray', data)
+        self.dataset = sima.ImagingDataset([seq], savedir=None)
+
+    def test_polygon_roi(self):
+        roi = sima.ROI.ROI(
+            polygons=[[2, 1, 1], [6, 1, 1], [6, 4, 1], [2, 4, 1]],
+            im_shape=(2, 6, 8))
+        rois = sima.ROI.ROIList([roi])
+        signals = self.dataset.extract(
+            rois=rois, signal_channel=0, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+
+        assert_array_equal(signals['raw'][0], 1.)
+
+    def test_boolean_mask_roi(self):
+        mask = np.array([[[False, False, False, False, False, False],
+                          [False, False, False, False, False, False],
+                          [False, False, False, False, False, False],
+                          [False, False, False, False, False, False]],
+                         [[False, False, False, False, False, False],
+                          [False, False, True, True, True, True],
+                          [False, False, True, True, True, True],
+                          [False, False, True, True, True, True]]])
+        roi = sima.ROI.ROI(mask=mask)
+        rois = sima.ROI.ROIList([roi])
+        signals = self.dataset.extract(
+            rois=rois, signal_channel=0, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+
+        assert_array_equal(signals['raw'], 1.)
+
+    def test_non_boolean_mask_roi(self):
+        mask = np.array([[[0., 0., 0., 0., 0., 0.],
+                          [0., 0., 0., 0., 0., 0.],
+                          [0., 0., 0., 0., 0., 0.],
+                          [0., 0., 0., 0., 0., 0.]],
+                         [[0., 0., 0., 0., 0., 0.],
+                          [0., 0., 0.4, 0.3, 0.2, 0.1],
+                          [0., 0., 0.4, 0.3, 0.2, 0.1],
+                          [0., 0., 0.4, 0.3, 0.2, 0.1]]])
+        roi = sima.ROI.ROI(mask=mask)
+        rois = sima.ROI.ROIList([roi])
+        signals = self.dataset.extract(
+            rois=rois, signal_channel=0, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+
+        expected_output = np.array(
+            [sum(np.roll(np.array([-0.6, -0.2, 0.2, 0.6]), t, 0)
+             * np.array([0.4, 0.3, 0.2, 0.1])) * 3. + 3. for t in range(20)])
+
+        assert_array_almost_equal(signals['raw'][0][0], expected_output)
+
+    def test_overlapping_rois(self):
+        polygon = [[2, 1, 1], [5, 1, 1], [5, 3, 1], [2, 3, 1]]
+        mask = np.array([[[False, False, False, False, False, False],
+                          [False, False, False, False, False, False],
+                          [False, False, False, False, False, False],
+                          [False, False, False, False, False, False]],
+                         [[False, False, False, False, False, False],
+                          [False, False, False, False, False, False],
+                          [False, False, False, True, True, True],
+                          [False, False, False, True, True, True]]])
+        rois = sima.ROI.ROIList(
+            [sima.ROI.ROI(polygons=polygon, im_shape=(2, 6, 8)),
+             sima.ROI.ROI(mask=mask)])
+        no_overlap_signals = self.dataset.extract(
+            rois=rois, signal_channel=0, remove_overlap=True, n_processes=2,
+            demix_channel=None)
+        overlap_signals = self.dataset.extract(
+            rois=rois, signal_channel=0, remove_overlap=False, n_processes=2,
+            demix_channel=None)
+
+        roi1_expected_no_overlap = np.array(
+            [sum(np.array([-0.6, -0.2, 0.2, 0.6])
+             * np.roll(np.array([0.5, 0.25, 0.25, 0.0]), -t, 0)) + 1.
+             for t in range(20)])
+        roi2_expected_no_overlap = np.array(
+            [sum(np.array([-0.6, -0.2, 0.2, 0.6])
+             * np.roll(np.array([0.0, 0.25, 0.25, 0.5]), -t, 0)) + 1.
+             for t in range(20)])
+        roi1_expected_overlap = np.array(
+            [sum(np.array([-0.6, -0.2, 0.2, 0.6])
+             * np.roll(np.array([1 / 3., 1 / 3., 1 / 3., 0.0]), -t, 0)) + 1.
+             for t in range(20)])
+        roi2_expected_overlap = np.array(
+            [sum(np.array([-0.6, -0.2, 0.2, 0.6])
+             * np.roll(np.array([0.0, 1 / 3., 1 / 3., 1 / 3.]), -t, 0)) + 1.
+             for t in range(20)])
+
+        assert_array_almost_equal(
+            no_overlap_signals['raw'][0][0], roi1_expected_no_overlap)
+        assert_array_almost_equal(
+            no_overlap_signals['raw'][0][1], roi2_expected_no_overlap)
+        assert_array_almost_equal(
+            overlap_signals['raw'][0][0], roi1_expected_overlap)
+        assert_array_almost_equal(
+            overlap_signals['raw'][0][1], roi2_expected_overlap)
+
+
+class Test_MissingData(object):
+    def setup(self):
+        return
+
+    def test_empty_roi(self):
+        return
+
+    def test_missing_frame(self):
+        return
+
+    def test_partial_missing_data(self):
+        return
+
+    def test_partial_missing_data_overlapping_rois(self):
+        return
 
 if __name__ == "__main__":
     run_module_suite()