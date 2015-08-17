9dd5fb311c1fe80df5400938332b84d885256849
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2015-08-17 17:16:30 +0100

    Add license and author information for epnev's method

--- sima/spikes.py
+++ sima/spikes.py
@@ -1,3 +1,22 @@
+"""
+epnev's method is based on code by Eftychios Pnevmatikakis:
+https://github.com/epnev/constrained_foopsi_python
+
+which was published under the GPL v2:
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see http://www.gnu.org/licenses/.
+"""
+
 from __future__ import division
 from __future__ import print_function
 from builtins import str
