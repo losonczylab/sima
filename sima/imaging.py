645e0614996542dcbb180956883f20aa2f009058
Vijay Mohan <vjlbym@gmail.com>
2016-01-04 18:38:42 -0500

    Added an export of standard deviation to the dataset

--- sima/imaging.py
+++ sima/imaging.py
@@ -290,6 +290,41 @@ class ImagingDataset(object):
         return self._time_averages
 
     @property
+    def time_std(self):
+        if hasattr(self, '_time_std'):
+            return self._time_std
+        if self.savedir is not None:
+            try:
+                with open(join(self.savedir, 'time_std.pkl'),
+                          'rb') as f:
+                    time_std = pickle.load(f)
+            except IOError:
+                pass
+            else:
+                # Same protocol as the averages. Make sure the
+                # std is a single 4D (zyxc) array and if not just 
+                # re-calculate the time std.
+                if isinstance(time_std, np.ndarray):
+                    self._time_std = time_std
+                    return self._time_std
+
+        sums = np.zeros(self.frame_shape)
+        sums_squares = np.zeros(self.frame_shape)
+        counts = np.zeros(self.frame_shape)
+        for frame in it.chain.from_iterable(self):
+            sums += np.nan_to_num(frame)
+            sums_squares += np.square(np.nan_to_num(frame))
+            counts[np.isfinite(frame)] += 1
+        means = old_div(sums, counts)
+        mean_of_squares = old_div(sums_squares, counts)
+        std = np.sqrt(mean_of_squares-np.square(means))
+        if self.savedir is not None and not self._read_only:
+            with open(join(self.savedir, 'time_std.pkl'), 'wb') as f:
+                pickle.dump(std, f, pickle.HIGHEST_PROTOCOL)
+        self._time_std = std
+        return self._time_std
+
+    @property
     def ROIs(self):
         try:
             with open(join(self.savedir, 'rois.pkl'), 'rb') as f:
@@ -522,7 +557,7 @@ class ImagingDataset(object):
             else:
                 os.remove(join(self.savedir, 'rois.pkl'))
 
-    def export_averages(self, filenames, fmt='TIFF16', scale_values=True):
+    def export_averages(self, filenames, fmt='TIFF16', scale_values=True, mean_or_std='mean'):
         """Save TIFF files with the time average of each channel.
 
         For datasets with multiple frames, the resulting TIFF files
@@ -538,6 +573,9 @@ class ImagingDataset(object):
         scale_values : bool, optional
             Whether to scale the values to use the full range of the
             output format. Defaults to False.
+        mean_or_std : {'mean', 'std'}, optional
+            Whether to take mean z projection or the std z projection.
+            The latter is useful for finding the dynamic pixels
 
         """
 
@@ -552,22 +590,30 @@ class ImagingDataset(object):
             if not h5py_available:
                 raise ImportError('h5py >= 2.2.1 required')
             f = h5py.File(filenames, 'w')
-            im = self.time_averages
+            if mean_or_std=='mean':
+                im = self.time_averages
+            elif mean_or_Std=='std':
+                im = self.time_std
+            else:
+                raise ValueError("mean_or_std must be 'mean' or 'std'")
             if scale_values:
                 im = sima.misc.to16bit(im)
             else:
                 im = im.astype('uint16')
-            f.create_dataset(name='time_average', data=im)
+            f.create_dataset(name='time_'+mean_or_std, data=im)
             for idx, label in enumerate(['z', 'y', 'x', 'c']):
-                f['time_average'].dims[idx].label = label
+                f['time_'+mean_or_std].dims[idx].label = label
             if self.channel_names is not None:
-                f['time_average'].attrs['channel_names'] = [
+                f['time_'+mean_or_std].attrs['channel_names'] = [
                     np.string_(s) for s in self.channel_names]
                 # Note: https://github.com/h5py/h5py/issues/289
             f.close()
         else:
             for chan, filename in enumerate(filenames):
-                im = self.time_averages[:, :, :, chan]
+                if mean_or_std=='mean':
+                    im = self.time_averages[:, :, :, chan]
+                elif mean_or_Std=='std':
+                    im = self.time_std[:, :, :, chan]
                 if dirname(filename):
                     mkdir_p(dirname(filename))
                 if fmt == 'TIFF8':
